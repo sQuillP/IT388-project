819cd9b61c05851ae4646ff3b31f51180d8584f3
wmpatti <willpattisons@yahoo.com>
2020-11-27 00:02:38 -0600

    updated Dealer.h file

--- Dealer.h
+++ Dealer.h
@@ -1,20 +1,27 @@
 #include "Player.h"
 #ifndef DEAL_H
 #define DEAL_H
+
+
 /*
-* Header file for dealer functions.
+* Serial Gang Software
+* Copyright 2020
+* @ William Pattison, Kevin Gomez,
+* and Filip Sofeski
 */
 
+
+/*Structure for the dealer, only contains a single hand*/
 typedef struct Dealer
 {
   Hand deck;
 } dealer_t;
 
 
-
 /*Functions for the dealer*/
 void createDealer(dealer_t* dealer);
 void clearDealer(dealer_t* dealer);
 void printDealerHand(dealer_t* dealer);
+
 
 #endif
